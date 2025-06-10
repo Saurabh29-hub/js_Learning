24f1fe02ae1dd5b4b234819afa7fb07515aae828
Saurabh Kumar <76895770+Saurabh29-hub@users.noreply.github.com>
2025-06-10 17:29:03 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 144553
+let accountEmail= "saurabh_2411cs12@iitp.ac.in"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail= "imsaurabhkumar29@gmail.com"
+accountPassword= "21212121"
+accountCity= "Bengaluru"
+
+console.log(accountId);
+/* Prefer not use var // value account id change above in this
+because of issue in block scope and Functional scope */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
