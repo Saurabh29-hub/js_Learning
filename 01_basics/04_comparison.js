fbfea7df06fdcf83ca94a22879ea8638af6492bc
Saurabh Kumar <76895770+Saurabh29-hub@users.noreply.github.com>
2025-06-11 17:30:21 +0000

    container comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // on comparing its data type conversion take place automatically
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  Strict check in this data conversion not take place as it is it compare
+
+console.log("2" === 2);
