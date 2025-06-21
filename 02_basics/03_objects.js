7e2bc96f4eacac8053c27f44a098f437ec4e1f51
Saurabh Kumar <76895770+Saurabh29-hub@users.noreply.github.com>
2025-06-21 19:09:33 +0000

    container

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,8 +3,19 @@
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1")  //symbols are unique
 
+const secret = Symbol("secret");
+
+const user = {
+  name: "Alice",
+  [secret]: "hiddenValue"
+};
+
+console.log(user.name);           // Alice
+console.log(user[secret]);        // hiddenValue
+console.log(Object.keys(user));   // ["name"]
+console.log(Object.getOwnPropertySymbols(user)); // [Symbol(secret)]
 
 const JsUser = {
     name: "Hitesh",
@@ -20,14 +31,14 @@ const JsUser = {
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"]) // access element using by [] bracket.
-// console.log(JsUser[mySym])
+console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)  // after that we are not able to update any element of objects
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
+JsUser.greeting = function(){    // add greeting function in JsUser Object
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
